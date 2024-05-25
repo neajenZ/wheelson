bed815de47f35468af062123ce769bc2ed8172e9
Alexandr Alexandrov <realumnyashkin@yandex.ru>
2024-05-25 21:27:35 +0300

    l

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from './shared/store/store.js';
 import './index.css'
@@ -9,7 +9,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename='/wheelsOn'>
           <App />
       </BrowserRouter>
     </Provider>
