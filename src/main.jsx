3152d5d53874570499a4c2c710245c6523fbf870
Alexandr Alexandrov <realumnyashkin@yandex.ru>
2024-05-26 10:34:26 +0300

    lala

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {HashRouter as Router} from "react-router-dom";
+import {createBrowserRouter, HashRouter as Router, RouterProvider} from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from './shared/store/store.js';
 import './index.css'
+import {LoginPage} from "./pages/login/login.jsx";
+import {Profile} from "./pages/profile/profile.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: '/wheelson/',
+        element: (
+            <Provider store={store}>
+                <App />
+            </Provider>
+        ),
+        children: [
+            {
+                path: '/wheelson/auth',
+                element: <LoginPage />
+            },
+            {
+                path: '/wheelson/profile',
+                element: <Profile />
+            }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <RouterProvider router={router} >
+
+      </RouterProvider>
     <Router>
-      <Provider store={store}>
-          <App />
-      </Provider>
+
     </Router>
       
   </React.StrictMode>,
