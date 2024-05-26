916edadfe8741cca668c5fb3f43cd8e910b89c9a
Alexandr Alexandrov <realumnyashkin@yandex.ru>
2024-05-26 08:49:21 +0300

    alo

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
           <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
       
   </React.StrictMode>,
