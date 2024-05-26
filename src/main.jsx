a3a40cc584d91fc391d01a67e84d304a543fae64
Alexandr Alexandrov <realumnyashkin@yandex.ru>
2024-05-26 10:20:02 +0300

    abra

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from './shared/store/store.js';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <Provider store={store}>
           <App />
       </Provider>
-    </HashRouter>
+    </Router>
       
   </React.StrictMode>,
 )
